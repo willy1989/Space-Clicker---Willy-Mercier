95de84becc5665b5ab617699beffc6f6d495ef55
Willy <willy.winelover@gmail.com>
2022-02-09 11:32:43 +0100

    Added game over sound effect

--- Assets/Scripts/GameOverLine.cs
+++ Assets/Scripts/GameOverLine.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class GameOverLine : MonoBehaviour
 {
+    [SerializeField] private SoundPlayer soundPlayer;
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(Constants.Target_Tag) == true)
+        {
+            soundPlayer.PlaySoundEffect();
             GameLoopManager.Instance.GameOver();
+        }
+            
     }
 }
