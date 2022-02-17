be3a8412033455afedafe69b3158353a577182a1
Willy <willy.winelover@gmail.com>
2022-02-17 15:26:47 +0100

    Disable input when clicking on a interactable UI element

--- Assets/Scripts/SpaceShip/SpaceShipInput.cs
+++ Assets/Scripts/SpaceShip/SpaceShipInput.cs
@@ -2,62 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class SpaceShipInput : Singleton<SpaceShipInput>
 {
-    private Vector3 destination;
-
-    public Vector3 Destination
-    {
-        get
-        {
-            return destination;
-        }
-    }
-
-    private bool canRegisterInput = true;
-
-    [SerializeField] private Button openShopButton;
-
-    [SerializeField] private Button closeShopButton;
-
+    public Vector2? PositionInput { get; private set; }
 
     private void Awake()
     {
         SetInstance();
     }
 
-    private void Start()
-    {
-        openShopButton.onClick.AddListener(ToggleInput);
-        closeShopButton.onClick.AddListener(ToggleInput);
-    }
-
     private void Update()
     {
-        RegisterDestinationInput();
+        PositionInput = RegisterDestinationInput();
     }
 
-    private void RegisterDestinationInput()
+    private Vector2? RegisterDestinationInput()
     {
-        if (Input.touchCount <= 0 || canRegisterInput == false)
-            return;
+        if (Input.touchCount <= 0)
+            return null;
 
         Touch touch = Input.GetTouch(0);
 
-        if(touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId) == true)
+            return null;
+
+        else
         {
-            Vector3 temp = Camera.main.ScreenToWorldPoint(touch.position);
-            destination = new Vector3(temp.x, temp.y, 0f);
+            return (Vector2)Camera.main.ScreenToWorldPoint(touch.position);
         }
-
-        if (touch.phase == TouchPhase.Ended)
-            destination = Vector3.zero;
-    }
-
-    private void ToggleInput()
-    {
-        canRegisterInput = !canRegisterInput;
     }
 }
