540f1e9bd7e24b160ac747067de98d97316b41e4
Willy <willy.winelover@gmail.com>
2022-03-09 12:45:11 +0100

    Fixed bug

--- Assets/Scripts/SpaceShip/SpaceShipMovement.cs
+++ Assets/Scripts/SpaceShip/SpaceShipMovement.cs
@@ -6,28 +6,37 @@ public class SpaceShipMovement : MonoBehaviour
 {
     private Vector3 idlePosition = new Vector3(0f, -1f, 0f);
 
-    private int movementSpeed = 25;
+    private int movementSpeed = 35;
 
     private Vector3 fingerPositionOffset = new Vector3(0f, 1.5f,0f);
 
-    private void FixedUpdate()
+    private float movementThreshold = 1f;
+
+    private void Update()
     {
         MoveToDestination();
     }
 
     private void MoveToDestination()
     {
-        Vector3 spaceShipToDestination;
+        Vector3 destination;
 
+        // When the player is not controlling the spaceship, we return it to its idle position
         if (SpaceShipInput.Instance.PositionInput == null)
-            spaceShipToDestination = idlePosition - transform.position;
+            destination = idlePosition;
 
         else
-            spaceShipToDestination = (Vector3)SpaceShipInput.Instance.PositionInput + fingerPositionOffset - transform.position;
+            destination = (Vector3)SpaceShipInput.Instance.PositionInput + fingerPositionOffset;
 
-        if (spaceShipToDestination.magnitude < 0.3f)
+        if (destination == transform.position)
             return;
 
-        transform.position += spaceShipToDestination.normalized * movementSpeed * Time.fixedDeltaTime;
+        Vector3 spaceShipToDestination = destination - transform.position;
+
+        if (spaceShipToDestination.magnitude < movementThreshold)
+            transform.position = destination;
+
+        else
+            transform.position += spaceShipToDestination.normalized * movementSpeed * Time.deltaTime;
     }
 }
